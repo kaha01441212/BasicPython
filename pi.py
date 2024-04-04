5eda7bad3a78df9c474a54225f0795a8dac33eba
kawamata haruka <soroban1212@icliud.com>
2024-04-04 11:57:05 +0900

    文字列

--- pi.py
+++ pi.py
@@ -4,4 +4,14 @@ text = """
 """
 
 # TODO
+word_list = text.split()
 
+list_1=list(map(len, word_list))
+
+count=0
+ans = 0
+for i in list_1:
+    ans+=i*0.1**(count)
+    count+=1
+    
+print (ans)
