b2cf67b3d0eb9f53a9310975cb321335af10674d
kawamata haruka <soroban1212@icliud.com>
2024-04-12 11:01:08 +0900

    素数判定

--- prime_number.py
+++ prime_number.py
@@ -6,35 +6,21 @@ b = input("bの値を入力: ")
 a = int(a)
 b = int(b)
 
-sosu = True
-if a <= 1:
-    sosu = False
-else:
-    i = 2
-    while i < a:
-        if a % i == 0:
-            sosu = False
-            break
-        i += 1
+def soha(a):
+    sosu = True
+    if a <= 1:
+        sosu = False
+    else:
+        i = 2
+        while i < a:
+            if a % i == 0:
+                sosu = False
+                break
+            i += 1
+    if sosu:
+        return "素数です"  
+    else:
+        return "素数ではありません"  
 
-if sosu:
-    print("素数です")
-else:
-    print("素数ではありません")
-
-sosu = True
-if b <= 1:
-    sosu = False
-else:
-    i = 2
-    while i < b:
-        if b % i == 0:
-            sosu = False
-            break
-        i += 1
-
-if sosu:
-    print("素数です")
-else:
-    print("素数ではありません")
-
+print(soha(a))
+print(soha(b))
