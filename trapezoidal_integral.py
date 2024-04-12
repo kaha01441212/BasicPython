4e00fad0b1e614ee89e5f0c4cf7e5a6da8b4ba32
kawamata haruka <soroban1212@icliud.com>
2024-04-12 10:46:58 +0900

    台形積分

--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,13 +1,27 @@
-from math import sin
+from math import sin,pi,exp
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
-s = 0
 import math
-for k in range(1, 101):
-    h = math.pi / (2 * k)
-    l = ((sin((k - 1) * h)) + (sin(k * h))) * 2 / h
-    s += l
-print(s)
 
+def msekibun(f,a,b,n):
+    h = (b-a)/n
+    s=0
+    l=0
+    for i in range(1,n+1):
+        l= f(a+(i-1)*h)+f(a+i*h)
+        s+=l
+    return s*(h/2)
+
+def f(x):
+    return sin(x)
+print(msekibun(f,0,pi/2,50))
+
+def g(x):
+    return 4/(1+x**2)
+print(msekibun(g,0,1,100))
+
+def k(x):
+    return (pi)**(1/2)*exp(-x**2)
+print(msekibun(k,-100,100,1000))
