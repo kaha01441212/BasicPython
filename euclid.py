4d16ab1a41381cb343239d3177ebca637c6cf3d6
kawamata haruka <soroban1212@icliud.com>
2024-04-12 11:12:59 +0900

    ユークリッド_修正

--- euclid.py
+++ euclid.py
@@ -2,20 +2,28 @@ a = input("a の値を入力: ")
 b = input("b の値を入力: ")
 
 # TODO
-def euclid(c,d):
-    c=int(c)
-    d=int(d)
-    if c<d:
-        c,d = d,c #c,dを入れ替える
+def euclid(c, d):
+    c = int(c)
+    d = int(d)
+    if c < d:
+        c, d = d, c 
     
     kari = 0
-    while d!=0:
-        kari = c%d
-        c=d
-        d=kari
+    while d != 0:
+        kari = c % d
+        c = d
+        d = kari
         
     return c
 
-number = euclid(a,b)
-print(number,"が最大公約数です。")
+def tagainiso(a, b):
+    if euclid(a, b) == 1:
+        return "互いに素です。"
+    else:
+        return "互いに素ではありません"
 
+number = euclid(a, b)
+print(number, "が最大公約数です。")
+
+kazu = tagainiso(a, b) 
+print(kazu)
